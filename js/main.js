deefb220b6a85142d917c278bc6c86abcfc55031
unknown <I:\TheBatPortable\Data\Mail>
2015-10-05 11:40:27 +0200

    project beállítása

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Elemek kiválasztása
+// @param selector:string  - Az elemet kiválasztó selector
+function _q( selector ){
+    // Elemek kiválasztása
+    var elements = document.querySelectorAll( selector );
+    
+    return elements;
+}
